c1a773f111f3345939642c0cc60259ea536c1354
ghvolshebnik <48733625+ghvolshebnik@users.noreply.github.com>
2020-11-15 17:21:39 +0300

    13 class is completed

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myfirstprogram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myfirstprogram.java
@@ -2,6 +2,25 @@ package ru.stqa.pft.sandbox;
 
 public class myfirstprogram {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("World");
+    hello("Artem!");
+
+    double l = 5;
+    System.out.println("Ploschad' kvadrata so storonoy " + l + " = " + area(l));
+
+    double a = 5;
+    double b = 7;
+    System.out.println("Ploschad' pryamougolnika so storonami " + a + " i " + b + " = " + area(a,b));
+  }
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area (double len) {
+    return len*len;
+  }
+
+  public static double area(double a, double b) {
+    return a*b;
   }
 }
